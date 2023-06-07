1985d9f80dee2f13d907a2a7a51b60aea65670a5
Anoop-coder <129997690+Anoop-coder@users.noreply.github.com>
2023-06-07 14:58:21 -0700

    erfsdfdsfsdf
    sdfsdfdsfsdf

--- Grade 8 group project/Assets/Scripts/SceneLoader.cs
+++ Grade 8 group project/Assets/Scripts/SceneLoader.cs
@@ -28,10 +28,7 @@ public class SceneLoader : MonoBehaviour
         SceneManager.LoadScene(3);
     }
 
-    public void LoadPlay2Scene()
-    {
-        SceneManager.LoadScene(5);
-    }
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -43,49 +40,68 @@ public class SceneLoader : MonoBehaviour
                
         
     }
+    public void LoadPlay2Scene()
+    {
+        SceneManager.LoadScene(5);
+    }
 
     public void LoadJaydenDialogueScene()
     {
-        SceneManager.LoadScene(7);
+        SceneManager.LoadScene(6);
     }
     public void LoadCutScene1()
     {
-        SceneManager.LoadScene(8);
+        SceneManager.LoadScene(7);
 
     }
 
     public void LoadCutScene2()
     {
-        SceneManager.LoadScene(9);
+        SceneManager.LoadScene(8);
     }
 
     public void LoadCut3Scene()
     {
-        SceneManager.LoadScene(10);
+        SceneManager.LoadScene(9);
     }
 
     public void LoadCutScene4()
     {
-        SceneManager.LoadScene(11);
+        SceneManager.LoadScene(10);
     }
     public void LoadCutScene5()
     {
-        SceneManager.LoadScene(12);
+        SceneManager.LoadScene(11);
     }
 
     public void LoadCutScene6()
     {
-        SceneManager.LoadScene(13);
+        SceneManager.LoadScene(12);
     }
 
     public void LoadSSTScene7()
     {
-        SceneManager.LoadScene(14);
+        SceneManager.LoadScene(13);
     }
 
     public void LoadJaydenScene()
     {
         SceneManager.LoadScene(14);
+    }
+
+    public void LoadTExtScene()
+    {
+        SceneManager.LoadScene(14);
+    }
+
+    public void LoadText2Scene()
+    {
+        SceneManager.LoadScene(15);
+    }
+
+    public void LoadTesxt4Scene()
+    {
+        SceneManager.LoadScene(16);
     }
 }
 
